b1a36a3b7f37bdd25b1dc68081f03b387c14d02d
Igor Kuznetsov <igkuznetsov@gmail.com>
2012-08-31 11:40:33 +0400

    Comments Migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120830064926) do
+ActiveRecord::Schema.define(:version => 20120830083740) do
+
+  create_table "comments", :force => true do |t|
+    t.string   "title"
+    t.string   "body"
+    t.integer  "users_id"
+    t.integer  "stories_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "stories", :force => true do |t|
     t.string   "title"
