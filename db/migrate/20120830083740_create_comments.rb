cecff452454632f0acb3431366275a44ba08e813
Igor Kuznetsov <igkuznetsov@gmail.com>
2012-08-31 11:38:33 +0400

    Comments Migration

--- db/migrate/20120830083740_create_comments.rb
+++ db/migrate/20120830083740_create_comments.rb
@@ -1,7 +1,10 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-
+	    t.string  :title
+	    t.string  :body
+	    t.references  :users
+	    t.references :stories
       t.timestamps
     end
   end
