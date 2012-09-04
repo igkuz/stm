0dc8a186f3b948c210d20954f2c20892b2e27ebc
Igor Kuznetsov <igkuznetsov@gmail.com>
2012-09-04 15:40:50 +0400

    adding and showing comments

--- app/controllers/web/stories/comments_controller.rb
+++ app/controllers/web/stories/comments_controller.rb
@@ -1,9 +1,21 @@
 class Web::Stories::CommentsController < Web::Stories::ApplicationController
   def new
+	  @story = Story.find(params[:story_id])
+  end
+
+  def create
+	  story = Story.find(params[:story_id])
+	  comment = story.comments.new(params[:comment])
+	  comment.user = current_user
+	  if comment.save
+		  redirect_to story_path story, :notice => "Comment added"
+	  else
+		  flash.now.alert "Smth went wrong"
+		  render story_path story
+	  end
   end
 
   def index
-	  #@comments = Comment.find
-	  #@comments = Comment.find_all_by_stories_id()
+
   end
 end
