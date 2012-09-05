2feaae3903cbf583a4ba109a17252c239e489e58
Igor Kuznetsov <igkuznetsov@gmail.com>
2012-09-05 11:07:59 +0400

    user's tests

--- test/functional/web/users_controller_test.rb
+++ test/functional/web/users_controller_test.rb
@@ -3,15 +3,48 @@ require 'test_helper'
 class Web::UsersControllerTest < ActionController::TestCase
 
 	def setup
-		user = create :user
-		if user
-			user_sign_in user
+		@user = create :user
+		if @user
+			user_sign_in @user
 		end
 	end
 
-	test "test_get_index" do
+	test "should get index" do
 		get :index
 		assert_response :success
+	end
+
+	test "should get new" do
+		get :new
+		assert_response :success
+	end
+
+	test "should post create" do
+		attrs = attributes_for(:user, :name => "TestName#2", :email => "1@mail.ru")
+		post :create, :user => attrs
+		assert_response :redirect
+
+		user = User.find_by_email(attrs[:email])
+		assert_not_nil user
+	end
+
+	test "should get show" do
+		get :show, :id => @user.id
+		assert_response :success
+	end
+
+	test "should get edit" do
+		get :edit, :id => @user.id
+		assert_response :success
+	end
+
+	test "should put update" do
+		attrs = attributes_for(:user, :name => "UpdateUser")
+		put :update, :id => @user.id, :user => attrs
+		assert_response :redirect
+
+		user = User.find(@user.id)
+		assert_equal(user.name, attrs[:name])
 	end
 
 end
